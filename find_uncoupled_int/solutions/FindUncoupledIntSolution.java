34fe7e38548edc67c4a6e80c4c327a8e2a8317c2
Alan Larkin <larkinalan@gmail.com>
2014-02-03 22:38:54 +0000

    add func to find all unpaired elements

--- find_uncoupled_int/solutions/FindUncoupledIntSolution.java
+++ find_uncoupled_int/solutions/FindUncoupledIntSolution.java
@@ -2,6 +2,8 @@ package solutions;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FindUncoupledIntSolution {
 
@@ -16,22 +18,38 @@ public class FindUncoupledIntSolution {
 	}
 
 	/*
-	 * Computes the bitwise-XOR, ^, of all the elements of an array to find the
-	 * unpaired element in O(n).
+	 * Computes the bitwise-XOR, ^, of all the elements of an array,
+	 * to find the unpaired element in O(n) time.
 	 * 
-	 * @param nums[] array of numbers to search in.
+	 * @param nums[] array of elements to search in.
 	 * 
 	 * @return unpaired element
 	 */
 	public static int find(int[] nums) {
-		int res = 0;
+		int unpaired = 0;
 		for (int i : nums)
-			res ^= i;
+			unpaired ^= i;
 
-		return res;
+		return unpaired;
 	}
 
-	public static int[] toIntArray(String line) {
+	/*
+	 * finds all the unpaired element in an array.
+	 * 
+	 * @param nums[] array of numbers to search in.
+	 * 
+	 * @return array of unpaired elements
+	 */
+	public static Integer[] findAll(int[] nums) {
+
+		Set<Integer> unpaired = new HashSet<Integer>();
+		for (int i : nums)
+			if (!unpaired.add(i)) unpaired.remove(i);
+
+		return unpaired.toArray(new Integer[unpaired.size()]);
+	}
+	
+	private static int[] toIntArray(String line) {
 		String[] nums = line.split(",");
 		int[] res = new int[nums.length];
 
