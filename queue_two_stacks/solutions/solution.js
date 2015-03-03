30de45b9ba63953f6ea4f8b6e4c12c123080ea2a
Brian Jordan <bcjordan@gmail.com>
2015-03-03 07:18:50 -0800

    add slow version

--- queue_two_stacks/solutions/solution.js
+++ queue_two_stacks/solutions/solution.js
@@ -1,24 +1,43 @@
-function Queue() {
+function Staqueue() {
   this.stackIn = [];
   this.stackOut = [];
 }
 
-Queue.prototype.enqueue = function (item) {
+Staqueue.prototype.fastEnqueue = function (item) {
   this.stackIn.push(item);
 }
 
-Queue.prototype.dequeue = function (item) {
+Staqueue.prototype.fastDequeue = function (item) {
   if (this.stackOut.length === 0) {
-    var inItem;
-    while (inItem = this.stackIn.pop()) {
-      this.stackOut.push(inItem);
-    }
+    this.reverseFromTo(this.stackIn, this.stackOut);
   }
 
   return this.stackOut.pop();
 }
 
-var queue = new Queue();
+Staqueue.prototype.enqueue = function (item) {
+  this.stackIn.push(item);
+};
+
+Staqueue.prototype.dequeue = function (item) {
+  this.reverseFromTo(this.stackIn, this.stackOut);
+
+  var node = this.stackOut.pop();
+
+  this.reverseFromTo(this.stackOut, this.stackIn);
+
+  return node;
+};
+
+Staqueue.prototype.reverseFromTo = function (from, to) {
+  var currentItem;
+  while (currentItem = from.pop()) {
+    to.push(currentItem);
+  }
+}
+
+
+var queue = new Staqueue();
 queue.enqueue('a');
 queue.enqueue('b');
 queue.enqueue('c');
@@ -31,3 +50,18 @@ console.log(queue.dequeue()); // c
 // stack flip required
 console.log(queue.dequeue()); // d
 console.log(queue.dequeue()); // e
+
+
+var queueFast = new Staqueue();
+queue.fastEnqueue('a');
+queue.fastEnqueue('b');
+queue.fastEnqueue('c');
+
+console.log(queue.fastDequeue()); // a
+console.log(queue.fastDequeue()); // b
+queue.fastEnqueue('d');
+queue.fastEnqueue('e');
+console.log(queue.fastDequeue()); // c
+// stack flip required
+console.log(queue.fastDequeue()); // d
+console.log(queue.fastDequeue()); // e
